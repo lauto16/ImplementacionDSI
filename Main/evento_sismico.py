91115bcf94c426a40389209b4f8d2cc0f672729e
lauto16 <lautymartino658@gmail.com>
2025-05-22 18:10:22 -0300

    clases base necesarias y creacion de ambos eventos sismologicos terminada

--- Main/evento_sismico.py
+++ Main/evento_sismico.py
@@ -7,19 +7,31 @@ class OrigenDeGeneracion:
         return self.nombre
 
 
+class ClasificacionSismo:
+    def __init__(
+        self, kmProfundidadDesde: float, kmProfundidadHasta: float, nombre: str
+    ) -> None:
+        self.kmProfundidadDesde = kmProfundidadDesde
+        self.kmProfundidadHasta = kmProfundidadHasta
+        self.nombre = nombre
+
+    def getNombre(self) -> str:
+        return self.nombre
+
+
 class MagnitudRichter:
     def __init__(self, descripcionMagnitud: str, numero: float) -> None:
         self.descripcionMagnitud = descripcionMagnitud
         self.numero = numero
-        
+
 
 class AlcanceSismo:
     def __init__(self, descripcion: str, nombre: str) -> None:
         self.descripcion = descripcion
         self.nombre = nombre
-    
+
     def getNombre(self) -> str:
-        return self.nombre  
+        return self.nombre
 
 
 class Estado:
@@ -28,9 +40,157 @@ class Estado:
         self.nombreEstado: str = nombreEstado
 
 
+class TipoDeDato:
+    def __init__(
+        self, denominacion: str, nombreUnidadMedida: str, valorUmbral: float
+    ) -> None:
+        self.denominacion = denominacion
+        self.nombreUnidadMedida = nombreUnidadMedida
+        self.valorUmbral = valorUmbral
+
+    def esTuDenominacion(self, denom_test) -> bool:
+        if denom_test == self.denominacion:
+            return True
+        return False
+
+
+class DetalleMuestraSismica:
+    def __init__(self, valor, tipoDeDato: TipoDeDato) -> None:
+        self.valor = valor
+        self.tipoDeDato = tipoDeDato
+
+    def getDatos(self) -> dict:
+        return {"valor": self.valor, "tipo_de_dato": self.tipoDeDato}
+
+
+class MuestraSismica:
+    def __init__(self, fechaHoraMuestra) -> None:
+        self.fechaHoraMuestra = fechaHoraMuestra
+        self.detallesMuestraSismica = []
+
+    def crearDetalleMuestra(self, valor, tipo_dato: TipoDeDato) -> None:
+        new_detalle = DetalleMuestraSismica(valor=valor, tipoDeDato=tipo_dato)
+        self.detallesMuestraSismica.append(new_detalle)
+
+
+class SerieTemporal:
+    def __init__(
+        self,
+        condicionAlarma: str,
+        fechaHoraInicioRegistroMuestras,
+        fechaHoraRegistros,
+        frecuenciaMuestreo: float,
+        estado: Estado,
+        muestrasSismica: list,
+    ) -> None:
+        self.condicionAlarma = condicionAlarma
+        self.fechaHoraInicioRegistroMuestras = fechaHoraInicioRegistroMuestras
+        self.fechaHoraRegistros = fechaHoraRegistros
+        self.frecuenciaMuestreo = frecuenciaMuestreo
+        self.estado = estado
+        self.muestrasSismica = muestrasSismica
+
+    def getDatos(self) -> dict:
+        return {
+            "condicionAlarma": self.condicionAlarma,
+            "fechaHoraInicioRegistroMuestras": self.fechaHoraInicioRegistroMuestras,
+            "fechaHoraRegistros": self.fechaHoraRegistros,
+            "frecuenciaMuestreo": self.frecuenciaMuestreo,
+            "estado": self.estado.nombreEstado,
+            #'muestrasSismica': self.muestrasSismica,
+        }
+
+
+class Empleado:
+    def __init__(self, apellido: str, mail: str, nombre: str, telefono: str) -> None:
+        self.apellido = apellido
+        self.mail = mail
+        self.nombre = nombre
+        self.telefono = telefono
+
+
+class Usuario:
+    def __init__(
+        self, contrasenia: str, nombreUsuario: str, empleado: Empleado
+    ) -> None:
+        # usar "ñ" es contra pep8
+        self.contrasenia = contrasenia
+        self.nombreUsuario = nombreUsuario
+        self.empleado = empleado
+
+
+class Sesion:
+    def __init__(self, usuario: Usuario) -> None:
+        self.usuario = usuario
+
+
+class CambioEstado:
+    def __init__(self, fechaHoraInicio, fechaHoraFin, estado: Estado, responsableInspeccion: Empleado) -> None:
+        self.fechaHoraFin = fechaHoraFin
+        self.fechaHoraInicio = fechaHoraInicio
+        self.estado = estado
+        self.responsableInspeccion = responsableInspeccion
+
+    def esEstadoActual(self) -> bool:
+        if self.fechaHoraFin == None:
+            return True
+        return False
+
+
+class EstacionSismologica:
+    def __init__(
+        self,
+        codigoEstacion,
+        documentoCertificacionAdq,
+        fechaSolicitudCertificacion,
+        latitud,
+        longitud,
+        nombre,
+        nroCertificacionAdquisicion,
+    ) -> None:
+        self.codigoEstacion = codigoEstacion
+        self.documentoCertificacionAdq = documentoCertificacionAdq
+        self.fechaSolicitudCertificacion = fechaSolicitudCertificacion
+        self.latitud = latitud
+        self.longitud = longitud
+        self.nombre = nombre
+        self.nroCertificacionAdquisicion = nroCertificacionAdquisicion
+
+
+class Sismografo:
+    def __init__(
+        self,
+        fechaAdquisicion,
+        identificadorSismografo,
+        nroSerie,
+        estacionSismologica: EstacionSismologica,
+    ) -> None:
+        self.fechaAdquisicion = fechaAdquisicion
+        self.identificadorSismografo = identificadorSismografo
+        self.nroSerie = nroSerie
+        self.estado = None
+        self.cambioEstado = None
+        self.estacionSismologica = estacionSismologica
+
+
 class EventoSismico:
-    def __init__(self, id: int, fechaHoraFin, fechaHoraOcurrencia, latitudEpicentro: float, longitudEpicentro: float,
-                 latitudHipocentro: float, longitudHipocentro: float, magnitud: MagnitudRichter, origenGeneracion: OrigenDeGeneracion, alcanceSismo: AlcanceSismo, estadoActual: Estado) -> None:
+    def __init__(
+        self,
+        id: int,
+        fechaHoraFin,
+        fechaHoraOcurrencia,
+        latitudEpicentro: float,
+        longitudEpicentro: float,
+        latitudHipocentro: float,
+        longitudHipocentro: float,
+        magnitud: MagnitudRichter,
+        origenGeneracion: OrigenDeGeneracion,
+        alcanceSismo: AlcanceSismo,
+        estadoActual: Estado,
+        clasificacion: ClasificacionSismo,
+        cambiosEstado: list,
+        seriesTemporales: list,
+    ) -> None:
         self.id = id
         self.fechaHoraFin = fechaHoraFin
         self.fechaHoraOcurrencia = fechaHoraOcurrencia
@@ -42,19 +202,39 @@ class EventoSismico:
         self.origenGeneracion = origenGeneracion
         self.alcanceSismo = alcanceSismo
         self.estadoActual = estadoActual
+        self.clasificacion = clasificacion
+        self.cambiosEstado = (cambiosEstado,)
+        self.seriesTemporales = seriesTemporales
 
     def as_dict(self):
         return {
-            'id': self.id,
-            'fechaHoraFin': self.fechaHoraFin.strftime('%Y-%m-%d %H:%M:%S') if hasattr(self.fechaHoraFin, 'strftime') else self.fechaHoraFin,
-            'fechaHoraOcurrencia': self.fechaHoraOcurrencia.strftime('%Y-%m-%d %H:%M:%S') if hasattr(self.fechaHoraOcurrencia, 'strftime') else self.fechaHoraOcurrencia,
-            'latitudEpicentro': self.latitudEpicentro,
-            'longitudEpicentro': self.longitudEpicentro,
-            'latitudHipocentro': self.latitudHipocentro,
-            'longitudHipocentro': self.longitudHipocentro,
-            'magnitud': self.magnitud.numero if hasattr(self.magnitud, 'numero') else self.magnitud,
-            'origenGeneracion': self.origenGeneracion.getNombre() if hasattr(self.origenGeneracion, 'getNombre') else self.origenGeneracion,
-            'alcanceSismo': self.alcanceSismo.getNombre(),
-            'estadoActual': self.estadoActual.nombreEstado
+            "id": self.id,
+            "fechaHoraFin": (
+                self.fechaHoraFin.strftime("%Y-%m-%d %H:%M:%S")
+                if hasattr(self.fechaHoraFin, "strftime")
+                else self.fechaHoraFin
+            ),
+            "fechaHoraOcurrencia": (
+                self.fechaHoraOcurrencia.strftime("%Y-%m-%d %H:%M:%S")
+                if hasattr(self.fechaHoraOcurrencia, "strftime")
+                else self.fechaHoraOcurrencia
+            ),
+            "latitudEpicentro": self.latitudEpicentro,
+            "longitudEpicentro": self.longitudEpicentro,
+            "latitudHipocentro": self.latitudHipocentro,
+            "longitudHipocentro": self.longitudHipocentro,
+            "magnitud": (
+                self.magnitud.numero
+                if hasattr(self.magnitud, "numero")
+                else self.magnitud
+            ),
+            "origenGeneracion": (
+                self.origenGeneracion.getNombre()
+                if hasattr(self.origenGeneracion, "getNombre")
+                else self.origenGeneracion
+            ),
+            "alcanceSismo": self.alcanceSismo.getNombre(),
+            "estadoActual": self.estadoActual.nombreEstado,
+            "clasificacion": self.clasificacion.getNombre(),
+            "cambiosEstado": [x[0].estado.nombreEstado for x in self.cambiosEstado],
         }
-
--- Main/views.py
+++ Main/views.py
@@ -1,4 +1,20 @@
-from .evento_sismico import EventoSismico, MagnitudRichter, OrigenDeGeneracion, AlcanceSismo, Estado
+from .evento_sismico import (
+    EventoSismico,
+    MagnitudRichter,
+    OrigenDeGeneracion,
+    AlcanceSismo,
+    Estado,
+    ClasificacionSismo,
+    CambioEstado,
+    SerieTemporal,
+    MuestraSismica,
+    TipoDeDato,
+    Sismografo,
+    EstacionSismologica,
+    Sesion,
+    Usuario,
+    Empleado
+)
 from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.shortcuts import render
 from datetime import datetime
@@ -6,7 +22,7 @@ from datetime import datetime
 
 def registrar_resultado_revision_manual(request: HttpRequest) -> HttpResponse:
     """
-    Guarda los resultados de la revision manual de un sismografo.
+    Vista que guarda los resultados de la revision manual de un sismografo.
     Args:
         request (HttpRequest): Peticion HTTP enviada por el usuario desde el frontend
 
@@ -19,16 +35,60 @@ def registrar_resultado_revision_manual(request: HttpRequest) -> HttpResponse:
         action = request.GET.get("action")
 
         if action == "get_eventos_sismicos":
-            # eventos de prueba ----------------------------------------------------------
+            # eventos de prueba ---------------------------------------------------------
 
             # estado para ambos eventos
-            no_revisado = Estado(ambito='EventoSismico', nombreEstado='NoRevisado')
+            no_revisado = Estado(ambito="EventoSismico", nombreEstado="NoRevisado")
 
-            fecha_inicio_evento1 = datetime.strptime("12/12/2004 12:45", "%d/%m/%Y %H:%M")
+            # sesion - usuario - empleado
+            empleado = Empleado(apellido='Doe',nombre='John', telefono='3516565784', mail='johndoe123@gmail.com')
+            user = Usuario(contrasenia='123456789', nombreUsuario='John Doe', empleado=empleado)
+            sesion = Sesion(usuario=user)
+
+            # sismografos
+            estacion_sismologica_evento1 = EstacionSismologica(codigoEstacion=1, documentoCertificacionAdq=None, fechaSolicitudCertificacion=None, latitud=-34.6037, longitud=-58.3816, nombre='Estacion Cordoba', nroCertificacionAdquisicion=10)
+            estacion_sismologica_evento2 = EstacionSismologica(codigoEstacion=2, documentoCertificacionAdq=None, fechaSolicitudCertificacion=None, latitud=31.5370, longitud=-68.5360, nombre='Estacion San Juan', nroCertificacionAdquisicion=11)
+        
+            sismografo_evento1 = Sismografo(estacionSismologica=estacion_sismologica_evento1, fechaAdquisicion=datetime.strptime("12/10/1998 12:30", "%d/%m/%Y %H:%M"), identificadorSismografo=1234, nroSerie=9089989898)
+            sismografo_evento2 = Sismografo(estacionSismologica=estacion_sismologica_evento2,fechaAdquisicion=datetime.strptime("12/10/1999 10:12", "%d/%m/%Y %H:%M"), identificadorSismografo=1235, nroSerie=9089989899)
+
+            fecha_inicio_evento1 = datetime.strptime(
+                "12/12/2004 12:45", "%d/%m/%Y %H:%M"
+            )
             fecha_fin_evento1 = datetime.strptime("12/12/2004 12:50", "%d/%m/%Y %H:%M")
-            magnitud_evento1 = MagnitudRichter('escala base', 7.1)
-            origen_de_generacion_evento1 = OrigenDeGeneracion('Capital zona rural', 'Cordoba')
-            alcance_evento1 = AlcanceSismo(nombre='Zonas aledañas camino Alta Gracia', descripcion='Zonas aledañas')
+            magnitud_evento1 = MagnitudRichter("escala base", 7.1)
+            origen_de_generacion_evento1 = OrigenDeGeneracion(
+                "Capital zona rural", "Cordoba"
+            )
+            alcance_evento1 = AlcanceSismo(
+                nombre="Zonas aledañas camino Alta Gracia", descripcion="Zonas aledañas"
+            )
+            clasificacion_sismo_evento1 = ClasificacionSismo(12.3, 15.6, "distancia")
+            cambio_estado_evento1 = CambioEstado(
+                fechaHoraInicio=fecha_inicio_evento1,
+                fechaHoraFin=None,
+                estado=no_revisado,
+                responsableInspeccion=empleado
+            )
+            estado_serie_temporal = Estado(
+                ambito="SerieTemporal", nombreEstado="SinAlarma"
+            )
+            muestra_sismica_evento1 = MuestraSismica(fecha_inicio_evento1)
+            tipo_dato_evento = TipoDeDato("altura_terreno", "metros", 150)
+            muestra_sismica_evento1.crearDetalleMuestra(
+                valor=120.54, tipo_dato=tipo_dato_evento
+            )
+            lista_muestras_sismicas_evento1 = [muestra_sismica_evento1]
+            serie_temporal_evento1 = SerieTemporal(
+                condicionAlarma="temblar",
+                fechaHoraInicioRegistroMuestras=fecha_inicio_evento1,
+                fechaHoraRegistros=fecha_fin_evento1,
+                frecuenciaMuestreo=1.5,
+                estado=estado_serie_temporal,
+                muestrasSismica=lista_muestras_sismicas_evento1,
+            )
+
+            lista_series_temporales_evento1 = [serie_temporal_evento1]
 
             evento1 = EventoSismico(
                 id=1,
@@ -39,16 +99,49 @@ def registrar_resultado_revision_manual(request: HttpRequest) -> HttpResponse:
                 latitudHipocentro=-30.6070,
                 longitudHipocentro=-50.1045,
                 magnitud=magnitud_evento1,
-                origenGeneracion= origen_de_generacion_evento1,
+                origenGeneracion=origen_de_generacion_evento1,
                 alcanceSismo=alcance_evento1,
-                estadoActual = no_revisado
+                estadoActual=no_revisado,
+                clasificacion=clasificacion_sismo_evento1,
+                cambiosEstado=[cambio_estado_evento1],
+                seriesTemporales=lista_series_temporales_evento1,
             )
 
-            fecha_inicio_evento2 = datetime.strptime("15/07/2010 09:30", "%d/%m/%Y %H:%M")
+            fecha_inicio_evento2 = datetime.strptime(
+                "15/07/2010 09:30", "%d/%m/%Y %H:%M"
+            )
             fecha_fin_evento2 = datetime.strptime("15/07/2010 09:45", "%d/%m/%Y %H:%M")
-            magnitud_evento2 = MagnitudRichter('escala base', 6.3)
-            origen_de_generacion_evento2 = OrigenDeGeneracion('Zona costera', 'San Juan')
-            alcance_evento2 = AlcanceSismo(nombre='Zonas aledañas tiramay', descripcion='Zonas aledañas')
+            magnitud_evento2 = MagnitudRichter("escala base", 6.3)
+            origen_de_generacion_evento2 = OrigenDeGeneracion(
+                "Zona costera", "San Juan"
+            )
+            alcance_evento2 = AlcanceSismo(
+                nombre="Zonas aledañas tiramay", descripcion="Zonas aledañas"
+            )
+            clasificacion_sismo_evento2 = ClasificacionSismo(20.4, 10.1, "distancia")
+            cambio_estado_evento2 = CambioEstado(
+                fechaHoraInicio=fecha_inicio_evento2,
+                fechaHoraFin=None,
+                estado=no_revisado,
+                responsableInspeccion=empleado
+            )
+
+            muestra_sismica_evento2 = MuestraSismica(fecha_inicio_evento2)
+            muestra_sismica_evento2.crearDetalleMuestra(
+                valor=1200.54, tipo_dato=tipo_dato_evento
+            )
+
+            lista_muestras_sismicas_evento2 = [muestra_sismica_evento2]
+            serie_temporal_evento2 = SerieTemporal(
+                condicionAlarma="temblar",
+                fechaHoraInicioRegistroMuestras=fecha_inicio_evento2,
+                fechaHoraRegistros=fecha_fin_evento2,
+                frecuenciaMuestreo=2.5,
+                estado=estado_serie_temporal,
+                muestrasSismica=lista_muestras_sismicas_evento2,
+            )
+
+            lista_series_temporales_evento2 = [serie_temporal_evento2]
 
             evento2 = EventoSismico(
                 id=2,
@@ -61,10 +154,15 @@ def registrar_resultado_revision_manual(request: HttpRequest) -> HttpResponse:
                 magnitud=magnitud_evento2,
                 origenGeneracion=origen_de_generacion_evento2,
                 alcanceSismo=alcance_evento2,
-                estadoActual = no_revisado
-        )
+                estadoActual=no_revisado,
+                clasificacion=clasificacion_sismo_evento2,
+                cambiosEstado=[cambio_estado_evento2],
+                seriesTemporales=lista_series_temporales_evento2,
+                
+            )
 
             eventos = [evento1.as_dict(), evento2.as_dict()]
+
             return JsonResponse(eventos, safe=False)
 
         else:
